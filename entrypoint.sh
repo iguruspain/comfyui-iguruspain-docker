60dfc0b5f9312f32272fd9142fcd0a7204f5a104
iguruspain <150747197+iguruspain@users.noreply.github.com>
2025-08-29 11:11:34 +0200

    Refactor entrypoint.sh to streamline setup process

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,48 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# This script is moved into the final image, so the original download logic is not needed.
+#if [ ! -f "/home/ubuntu/.download-complete" ] ; then
+#
+#echo "########################################"
+#echo "[INFO] Downloading ComfyUI & Manager..."
+#echo "########################################"
+#
+#set +e
+#cd /home/ubuntu
+#git clone https://github.com/comfyanonymous/ComfyUI.git
+#cd /home/ubuntu/ComfyUI
+#git clone https://github.com/ltdrdata/ComfyUI-Manager comfyui-manager
+#set -e
+#touch /home/ubuntu/.download-complete
+#
+#fi ;
 
 # Symbolic link
 if [ ! -f "/home/ubuntu/.link-wf" ] ; then
     if [ -d "/home/ubuntu/ComfyUI/user/default/workflows" ]; then
+        # Y que la carpeta de destino del enlace simbólico también exista
         if [ -d "/home/ubuntu/workflows" ]; then
             echo "#############################################"
             echo "[INFO] Making a symbolic link to Workflows..."
             echo "#############################################"
             echo ""
             echo ""
+            # Elimina el directorio original si existe, para reemplazarlo con el enlace
             rm -rf /home/ubuntu/ComfyUI/user/default/workflows
+            # Crea el enlace simbólico
             ln -s /home/ubuntu/workflows /home/ubuntu/ComfyUI/user/default/workflows
             touch /home/ubuntu/.link-wf
         fi
     fi
 fi
 
-# Activate the Python virtual environment and execute the ComfyUI server
+# Ensure .bashrc ends with source for venv
+BASHRC="/home/ubuntu/.bashrc"
+LINE="source ~/ComfyUI/.venv/bin/activate"
+
+if ! tail -n 1 "$BASHRC" | grep -Fxq "$LINE"; then
+    echo "$LINE" >> "$BASHRC"
+    echo "[INFO] Added virtualenv source line to .bashrc"
+fi
+
 . /home/ubuntu/ComfyUI/.venv/bin/activate && exec python3 /home/ubuntu/ComfyUI/main.py --listen --port 8188 ${CLI_ARGS}
