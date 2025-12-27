737df6c8662904e1bde9099591d8f9a81e72a011
iguruspain <iguruspain@cachyos-x8664>
2025-12-27 14:42:04 +0100

    fixed path

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,7 +30,7 @@ if [ ! -f "/home/ubuntu/.custom-nodes-installed" ] ; then
     . /home/ubuntu/ComfyUI/.venv/bin/activate
     #install nodes from custom_nodes.txt using ComfyUI-Manager cli
     #https://github.com/Comfy-Org/ComfyUI-Manager/blob/main/docs/en/cm-cli.md
-    python3 /home/ubuntu/ComfyUI/custom_nodes/cm-cli.py install -f /home/ubuntu/custom_nodes.txt -
+    python3 /home/ubuntu/ComfyUI/custom_nodes/comfy-manager/cm-cli.py install -f /home/ubuntu/custom_nodes.txt -
     touch /home/ubuntu/.custom-nodes-installed
 fi
 
